42c4b647c3d3500a0f621ca3c311a38a002164d1
Matias-DR <Matias-DR@outlook.com>
2022-11-27 22:30:42 -0300

    Add - var and hook definition

--- client/src/add_user.js
+++ client/src/add_user.js
@@ -1,4 +1,14 @@
+import {useState} from 'react';
+
 function AddUser() {
+
+    // Hooks - Data binding
+    const[name, setName] = useState('')
+    const[email, setEmail] = useState('')
+    const[tel, setTel] = useState('')
+
+    function add_user() {}
+
     return (
         <div class="container text-center">
             <div class="row">
@@ -8,15 +18,15 @@ function AddUser() {
                 <div class="col-sm-6 offset-3 border border-warning">
                     <div className="mt-1">
                         <label htmlFor="name" className="form-label">Nombre</label>
-                        <input type="text" className="form-control" value=""></input>
+                        <input type="text" className="form-control" value={name} onChange={(e) => {setName(e.target.value)}}></input>
                     </div>
                     <div className="mt-1">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input type="email" className="form-control" value=""></input>
+                        <input type="email" className="form-control" value={email} onChange={(e) => {setEmail(e.target.value)}}></input>
                     </div>
                     <div className="mt-1">
                         <label htmlFor="phone" className="form-label">Teléfono</label>
-                        <input type="tel" className="form-control" value=""></input>
+                        <input type="tel" className="form-control" value={tel} onChange={(e) => {setTel(e.target.value)}}></input>
                     </div>
                     <button onClick="" className="mt-3 mb-3 btn btn-success">Guardar usuario</button>
                 </div>
