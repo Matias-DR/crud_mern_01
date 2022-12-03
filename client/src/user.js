16fe8ea87edbbf3942f189520bebef5d0199ad5a
Matias-DR <Matias-DR@outlook.com>
2022-12-03 20:09:45 -0300

    Add - user data render

--- client/src/user.js
+++ client/src/user.js
@@ -1,9 +1,23 @@
-function User() {
+function User({ data }) {
     return (
-        <div>
-            <h2>
-                Usuario
-            </h2>
+        <div className="container text-center mt-3 mb-3 ">
+            <div className="row">
+                <div className="col">
+                    {data.name}
+                </div>
+                <div className="col">
+                    {data.email}
+                </div>
+                <div className="col">
+                    {data.phone}
+                </div>
+                <div className="col">
+                    <button type="button" className="btn btn-sm btn-warning">Editar</button>
+                </div>
+                <div className="col">
+                    <button type="button" class="btn btn-sm btn-outline-danger">Eliminar</button>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/users.js
+++ client/src/users.js
@@ -4,19 +4,32 @@ import User from './user'
 
 function Users() {
     const [users_data, set_users_data] = useState([])
+
     useEffect(() => {
         axios.get('api/user/get_users').then(res => {
-            console.log(res)
+            set_users_data(res.data)
         }).catch(
-            err => { console.log('_ERROR_: ', err)
+            err => { console.log('Petición fallida', err)
         })
     }, [])
+
+    const users_list = users_data.map(user => {
+        return (
+            // Es importante definir la pk del elemento cuando se lo define dentro de una lista de elementos. No se genera automáticamente una pk para cada elemento dentro de la lista de elementos
+            <div key={user.id}>
+                <User data={user}/>
+            </div>
+        )
+    })
+
     return (
         <div>
             <h1>
                 Lista de usuarios
             </h1>
-            <User/>
+            <div className="container text-center">
+                {users_list}
+            </div>
         </div>
     )
 }
