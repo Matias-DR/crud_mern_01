6c1ffb8910d15c405ba68f0a221a79f66c274363
Matias-DR <Matias-DR@outlook.com>
2022-12-03 20:09:22 -0300

    Fix - user format (attr tel → phone)

--- client/src/add_user.js
+++ client/src/add_user.js
@@ -7,13 +7,13 @@ function AddUser() {
     // Hooks - Data binding
     const[name, setName] = useState('')
     const[email, setEmail] = useState('')
-    const[tel, setTel] = useState('')
+    const[phone, setPhone] = useState('')
 
     function add_user() {
         var user = {
             name: name,
             email: email,
-            tel: tel,
+            phone: phone,
             id: uniqid()
         }
         // res.then funciona como un try except, captura la excepción levantada
@@ -40,7 +40,7 @@ function AddUser() {
                     </div>
                     <div className="mt-1">
                         <label htmlFor="phone" className="form-label">Teléfono</label>
-                        <input type="tel" className="form-control" value={tel} onChange={(e) => {setTel(e.target.value)}}></input>
+                        <input type="tel" className="form-control" value={phone} onChange={(e) => {setPhone(e.target.value)}}></input>
                     </div>
                     <button onClick={add_user} className="mt-3 mb-3 btn btn-success">Guardar usuario</button>
                 </div>
