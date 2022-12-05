467c23709a1bc5ba791a100baf6a656519e0f92e
Matias-DR <Matias-DR@outlook.com>
2022-12-04 23:46:39 -0300

    Add - commented http petition without Axios

--- client/src/users.js
+++ client/src/users.js
@@ -3,9 +3,36 @@ import { useState, useEffect } from 'react';
 import User from './user'
 
 function Users() {
-    const [users_data, set_users_data] = useState([])
+    const [users, set_users_data] = useState([])
 
     useEffect(() => {
+        /*
+        Si se quisiera realizar una petición sin utilizar Axios, podría utilizarse 'fetch' de la siguiente manera:
+        fetch('http://localhost:3000/api/user/get_users').then(
+            res => res.json()
+        ).then(
+            user_data => set_users_data(user_data)
+        ).catch(
+            error => console.log(error.message)
+        );
+
+        // Podrían realizarse otras peticiones configurando la función 'fetch' con el parámetro 'options' de la siguiente manera. Aclaración del lado del backend debería esperarse una función 'post('get_users')':
+        fetch(
+            'http://localhost:3000/api/user/get_users',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                // Contenido que desea enviarse como cuerpo, podría en ejemplo ser un id para realizar una búsqueda
+                // body: JSON.stringify('')
+            }
+        ).then(
+            res => res.json()
+        ).then(
+            user_data => set_users_data(user_data)
+        ).catch(
+            error => console.log(error.message)
+        );
+        */
         axios.get('api/user/get_users').then(res => {
             set_users_data(res.data)
         }).catch(
@@ -13,10 +40,10 @@ function Users() {
                 console.log('Petición fallida', err)
             })
     },
-    // Sobre esta lista, lista de dependencias, se indican las variables para las que se ejecutará el mismo método 'useEffect' cuando cambien de estado, por ejemplo, se elimina un usuario de la lista 'users_data', funcinoa como si se tratase de enlistarse en la variable como observador
-    [users_data])
+        // Sobre esta lista, lista de dependencias, se indican las variables para las que se ejecutará el mismo método 'useEffect' cuando cambien de estado, por ejemplo, se elimina un usuario de la lista 'users', funcinoa como si se tratase de enlistarse en la variable como observador
+        [users])
 
-    const users_list = users_data.map(user => {
+    const users_list = users.map(user => {
         return (
             // Es importante definir la pk del elemento cuando se lo define dentro de una lista de elementos. No se genera automáticamente una pk para cada elemento dentro de la lista de elementos
             <div key={user.id}>
