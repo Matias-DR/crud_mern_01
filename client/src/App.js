4a8deffb0da23d41ee58d14d0ffe609ce15a5d70
Matias-DR <Matias-DR@outlook.com>
2022-12-04 21:41:46 -0300

    Edit - unnecessary options removed

--- client/src/App.js
+++ client/src/App.js
@@ -17,24 +17,7 @@ function App() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/add_user">Add user</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/user_edit">Edit user</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
+                                <a className="nav-link active" aria-current="page" href="/add_user">Agregar usuario</a>
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
